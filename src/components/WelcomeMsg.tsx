39b99f304f78ed1d986762dc444d2dc1187c0096
Keeeparis <vladimir1trotsenko@gmail.com>
2022-03-31 14:07:58 +0600

    remove return, change props visual position

--- src/components/WelcomeMsg.tsx
+++ src/components/WelcomeMsg.tsx
@@ -1,16 +1,17 @@
-const WelcomeMsg = ({ account }: { account: boolean }) => {
-    return (
-        <>
-            {account ? (
-                <h2>Welcome to Metamask</h2>
-            ) : (
-                <>
-                    <h2>Metamask is locked.</h2>
-                    <h4>Please LogIn.</h4>
-                </>
-            )}
-        </>
-    )
-}
+const WelcomeMsg = ({ 
+    account 
+} : { 
+    account: boolean 
+}) => 
+    <>
+        {account ? (
+            <h2>Welcome to Metamask</h2>
+        ) : (
+            <>
+                <h2>Metamask is locked.</h2>
+                <h4>Please LogIn.</h4>
+            </>
+        )}
+    </>
 
 export default WelcomeMsg
